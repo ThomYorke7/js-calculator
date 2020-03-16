dd4e69581388ed23a663381191beacee59427882
Fabio Carrella <fabiocarrella@gmail.com>
2020-03-16 13:01:48 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const buttons = Array.from(document.getElementsByTagName("button"));
 const overallOperation = document.getElementById("operation");
 const currentOperation = document.getElementById("display");
-const operation = [];
+let operation = [];
 
 function add(a, b) {
     return a + b;
@@ -16,40 +16,74 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b == 0) {
+        return "Error"
+    } else {
+        return a / b;
+    }
 }
 
 function percentage(a) {
     return a / 100;
 }
 
+function roundNumber(num, dec) {
+    return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+}
+
+
+
 function recognizeOperator(operator, a, b) {
     switch (operator) {
         case "+":
             operation.length = 0;
             operation.push(add(a, b));
-            console.log(operation[0]);
+            console.log(operation);
             break;
         case "-":
-            console.log(a)
+            operation.length = 0;
+            operation.push(subtract(a, b));
+            console.log(operation);
+            break;
+        case "x":
+            operation.length = 0;
+            operation.push(multiply(a, b));
+            console.log(operation);
+            break;
+        case "/":
+            operation.length = 0;
+            operation.push(divide(a, b));
+            console.log(operation);
+            break;
     }
 }
 
-function roundNumber(num, dec) {
-    return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-}
+
+
 
 function operate() {
-    result = null
-    if (operation.length === 3) {
+    if (operation.length >= 3) {
         let operator = operation[1];
         let a = parseInt(operation[0]);
         let b = parseInt(operation[2]);
         recognizeOperator(operator, a, b);
-    } else {
-        console.log("b")
     }
 }
+
+
+function extractOperation(string) {
+    let arr = string.split(/(\D)/g);
+    if (operation.length == 0) {
+        operation.push(...arr)
+    }
+    else {
+        operation.push(...arr.slice(-2));
+        operate();
+    }
+    console.log(arr);
+    console.log(operation)
+}
+
 
 
 function populateDisplay() {
@@ -65,7 +99,35 @@ function populateDisplay() {
                     currentOperation.textContent = "";
                     break;
                 case ".":
-                    if (!overallOperation.textContent.includes(".")) {
+                    if (!currentOperation.textContent.includes(".")) {
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent = e.target.textContent;
+                    };
+                    break;
+                case "+":
+                    if (!currentOperation.textContent.includes("+")) {
+                        extractOperation(overallOperation.textContent);
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent = e.target.textContent;
+                    };
+                    break;
+                case "-":
+                    if (!currentOperation.textContent.includes("-")) {
+                        extractOperation(overallOperation.textContent);
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent = e.target.textContent;
+                    };
+                    break;
+                case "x":
+                    if (!currentOperation.textContent.includes("x")) {
+                        extractOperation(overallOperation.textContent);
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent = e.target.textContent;
+                    };
+                    break;
+                case "/":
+                    if (!currentOperation.textContent.includes("/")) {
+                        extractOperation(overallOperation.textContent);
                         overallOperation.textContent += e.target.textContent;
                         currentOperation.textContent = e.target.textContent;
                     };
@@ -73,14 +135,13 @@ function populateDisplay() {
                 default:
                     overallOperation.textContent += e.target.textContent;
                     currentOperation.textContent = e.target.textContent;
-                    operation.push(e.target.textContent)
-                    console.log(operation)
-                    operate();
+                // extractOperation(overallOperation.textContent)
+                // // operation.push(overallOperation.textContent.split(/(\D)/g))
+                // console.log(operation);
+                // operate();
             }
         })
     }
 }
 
 populateDisplay();
-
-operate();
