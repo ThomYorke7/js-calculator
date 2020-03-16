9769749493196c3e09297ff58fd26bad700ced9a
Fabio Carrella <fabiocarrella@gmail.com>
2020-03-16 18:26:15 +0100

    Finalize project

--- script.js
+++ script.js
@@ -1,8 +1,114 @@
+/* Global Variables */
 const buttons = Array.from(document.getElementsByTagName("button"));
 const overallOperation = document.getElementById("operation");
 const currentOperation = document.getElementById("display");
+let accumulator = [];
 let operation = [];
 
+
+/* Shows the user input on the calculator display */
+function populateDisplay() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", (e) => {
+            switch (e.target.textContent) {
+                case "C":
+                    overallOperation.textContent = "";
+                    currentOperation.textContent = "";
+                    operation.length = 0;
+                    break;
+                case "Del":
+                    overallOperation.textContent = overallOperation.textContent.slice(0, -1);
+                    currentOperation.textContent = "";
+                    operation.pop();
+                    break;
+                case ".":
+                    if (!currentOperation.textContent.includes(".") && overallOperation.textContent != "") {
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent += e.target.textContent;
+                    };
+                    break;
+                case "+":
+                case "-":
+                case "x":
+                case "/":
+                case "^":
+                    if (typeof (currentOperation.textContent == "number") && currentOperation.textContent != "") {
+                        extractOperation(overallOperation.textContent);
+                        overallOperation.textContent += e.target.textContent;
+                        currentOperation.textContent = "";
+                    };
+                    break;
+                case "=":
+                    if (typeof (currentOperation.textContent == "number") && overallOperation.textContent != "") {
+                        extractOperation(overallOperation.textContent);
+                        overallOperation.textContent = "";
+                        currentOperation.textContent = operation[0];
+                    };
+                    break;
+                default:
+                    overallOperation.textContent += e.target.textContent;
+                    currentOperation.textContent += e.target.textContent;
+            }
+        })
+    }
+}
+
+
+/* Parses the user input and passes the elements in two different arrays */
+function extractOperation(string) {
+    accumulator = string.split(/([+x/^=-])/g);
+    if (operation.length == 0) {
+        operation.push(...accumulator)
+    }
+    else {
+        operation.push(...accumulator.slice(-2));
+        operate();
+    }
+}
+
+
+/* Sets the operation when the "operation" array has two numbers and an operand */
+function operate() {
+    if (operation.length >= 3) {
+        filteredOperation = operation.filter(el => {
+            return el != ""
+        })
+        let operator = filteredOperation[1];
+        let a = parseFloat(filteredOperation[0]);
+        let b = parseFloat(filteredOperation[2]);
+        recognizeOperator(operator, a, b);
+    }
+}
+
+
+/* Recognizes the type of operation needed */
+function recognizeOperator(operator, a, b) {
+    switch (operator) {
+        case "+":
+            operation.length = 0;
+            operation.push(add(a, b));
+            break;
+        case "-":
+            operation.length = 0;
+            operation.push(subtract(a, b));
+            break;
+        case "x":
+            operation.length = 0;
+            operation.push(multiply(a, b));
+            break;
+        case "/":
+            operation.length = 0;
+            operation.push(divide(a, b));
+            break;
+        case "^":
+            operation.length = 0;
+            operation.push(power(a, b));
+            break;
+    }
+}
+
+
+/* Basic Operations */
 function add(a, b) {
     return a + b;
 }
@@ -23,107 +129,9 @@ function divide(a, b) {
     }
 }
 
-function percentage(a) {
-    return a / 100;
+function power(a, b) {
+    return a ** b;
 }
 
-function roundNumber(num, dec) {
-    return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-}
-
-
-
-function recognizeOperator(operator, a, b) {
-    switch (operator) {
-        case "+":
-            operation.length = 0;
-            operation.push(add(a, b));
-            console.log(operation);
-            break;
-        case "-":
-            operation.length = 0;
-            operation.push(subtract(a, b));
-            console.log(operation);
-            break;
-        case "x":
-            operation.length = 0;
-            operation.push(multiply(a, b));
-            console.log(operation);
-            break;
-        case "/":
-            operation.length = 0;
-            operation.push(divide(a, b));
-            console.log(operation);
-            break;
-    }
-}
-
-
-
-
-function operate() {
-    if (operation.length >= 3) {
-        let operator = operation[1];
-        let a = parseInt(operation[0]);
-        let b = parseInt(operation[2]);
-        recognizeOperator(operator, a, b);
-    }
-}
-
-
-function extractOperation(string) {
-    let arr = string.split(/(\D)/g);
-    if (operation.length == 0) {
-        operation.push(...arr)
-    }
-    else {
-        operation.push(...arr.slice(-2));
-        operate();
-    }
-    console.log(arr);
-    console.log(operation)
-}
-
-
-
-function populateDisplay() {
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", (e) => {
-            switch (e.target.textContent) {
-                case "C":
-                    overallOperation.textContent = "";
-                    currentOperation.textContent = "";
-                    break;
-                case "Del":
-                    overallOperation.textContent = overallOperation.textContent.slice(0, -1);
-                    currentOperation.textContent = "";
-                    break;
-                case ".":
-                    if (!currentOperation.textContent.includes(".")) {
-                        overallOperation.textContent += e.target.textContent;
-                        currentOperation.textContent = e.target.textContent;
-                    };
-                    break;
-                case "+":
-                case "-":
-                case "x":
-                case "/":
-                    if (currentOperation.textContent >= 0 && currentOperation.textContent <= 9) {
-                        extractOperation(overallOperation.textContent);
-                        overallOperation.textContent += e.target.textContent;
-                        currentOperation.textContent = e.target.textContent;
-                    };
-                    break;
-                default:
-                    overallOperation.textContent += e.target.textContent;
-                    currentOperation.textContent = e.target.textContent;
-                // extractOperation(overallOperation.textContent)
-                // // operation.push(overallOperation.textContent.split(/(\D)/g))
-                // console.log(operation);
-                // operate();
-            }
-        })
-    }
-}
 
 populateDisplay();
